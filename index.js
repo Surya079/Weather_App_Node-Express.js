b2c54c21eeef4c3fdc51b8498cd390619671d2da
Surya <surya.vme005@gmail.com>
2024-06-23 10:11:47 +0530

    get Extrenal api and get body request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
+import { name } from 'ejs';
 
 const app = express();
 const port = 3000;
@@ -9,22 +10,32 @@ const API_URL = "https://api.openweathermap.org/data/2.5/"
 
 // "weather?q={city name}&appid={API key}"
 
+///// BodyRequest package ///////
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+
 ///// Config Our STATIC FILE /////
 app.use(express.static("public"));
 
 
 
 /////// Get Routes of Weather//////
-
-app.get('/', async (req, res)=>{
-    const City = req.body
-    try {
-        const result = await axios.post(API_URL + `weather?q=${City}&appid=${OPENWEATHERMAP_API_KEY}`)
-    } catch (error) {
-        
-    }
+app.get('/', (req, res)=>{
+    
+    res.render("index.ejs")
 })
-
+app.post('/',async (req, res)=>{
+    const City = req.body["City"]
+   try {
+    const result = await axios.get(API_URL + `weather?q=${City}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`);
+    res.render("index.ejs", { content : JSON.stringify(result.data) });
+} catch (error) {
+    res.render("index.ejs", { content : error.message});
+   }    
+    
+})
 
 
 ////// Server Config /////////
